5209535b8d7b45d3335377b9c4343e70455cd768
Tanatar Absattarov <tanatar@MacBook-Pro.local>
2020-05-29 12:16:30 +0600

    update

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
         <div class="carousel-inner">
 		<div class="carousel-item active">
 		<div class="mainbanner h-100" style="background-image:url('images/tk_banner.png')">
-			<div class="container">
+			<div class="container h-100">
 			<div class="row align-items-end">
 					<a href="http://b-idea.kz/trudovoi_kodeks_k.php" target="_blank" class="register" style="width:auto;">Подробнее</a>
 			</div>
