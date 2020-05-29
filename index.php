de888b8d031b2f1bf8d2c2fa2989018ab08ce251
Tanatar Absattarov <tanatar@MacBook-Pro.local>
2020-05-29 11:10:06 +0600

    update

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
             <div class="container">
             <div class="row">
             <div class="col-md-3"></div>
-            <div class="col-md-6 h-100 align-bottom justify-content-end"><a href="http://b-idea.kz/trudovoi_kodeks_k.php" target="_blank" class="register">Подробнее</a></div>
+            <div class="col-md-6 h-100 d-flex align-content-end align-bottom justify-content-end"><a href="http://b-idea.kz/trudovoi_kodeks_k.php" target="_blank" class="register">Подробнее</a></div>
             <div class="col-md-3"></div>
             </div>
         </div>
