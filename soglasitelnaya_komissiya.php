242c58671696043da7b724582c31032382c901f2
Tanatar Absattarov <tanatar@MacBook-Pro.local>
2020-06-24 10:50:15 +0600

    update

--- soglasitelnaya_komissiya.php
+++ soglasitelnaya_komissiya.php
@@ -5,7 +5,7 @@
     <main class="template">
     <div class="mainbanner" style="background-image:url('images/online_seminar_t_s_g.jpg'); height:550px;">
 		</div>
-    <div class="container content">
+    <div class="container content" style="margin-top:0;">
 	<div class="row">
 	<div class="col-md-12">
 					<center><a href="https://form.jotform.com/62335883866166" target="_blank" class="register px-4 w-50">Принять участие</a></center>
