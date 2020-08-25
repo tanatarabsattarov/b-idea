03916fb7aa276b143257f6f9a9efce6ee0666973
Tanatar <tanatar777@mail.ru>
2020-08-25 15:51:54 +0600

    update

---
+++ mfso.php
@@ -0,0 +1,126 @@
+<?php 
+    $pageTitle = "Новое в МСФО: последние изменения и практические аспекты";
+    include("includes/header.html"); ?>
+
+    <main class="template">
+    <div class="mainbanner" style="background-image:url('images/seminar19-20.jpg'); height:450px;">
+			<div class="d-flex h-100 align-items-end">
+					<a href="https://form.jotform.com/62335883866166" target="_blank" class="register px-4 w-50">Принять участие</a>
+			</div>
+		</div>
+    </div>
+    <div class="container content">
+        <div class="row">
+            <div class="col-md-3">
+        <h3>Цель</h3>
+            </div>
+        <div class="col-md-9 boxborder">
+        <ul>
+            <li>получение слушателями информации о последних изменениях и практических аспектх Международных стандартов финансовой отчетности;</li>
+        </ul>
+           </div></div>  
+        <hr>        
+        <div class="row">
+            <div class="col-md-3">
+        <h3>Содержание</h3>
+            </div>
+             <div class="col-md-9 boxborder">
+    <ol>
+    <li>Новое в МСФО в 2020 году</li>
+    <ul>
+        <li>Новое определение бизнеса МСФО (IFRS) 3 «Объединения бизнесов»</li>
+        <li>Новое понятие существенности в финансовой отчетности</li>
+        <li>Реформа внутрибанковской ставки рефинансирования</li>
+    </ul>
+    <li>Поправки к стандартам (краткий обзор) с 1 января 2019 года</li>
+    <ul>
+        <li>МСФО (IFRS) 3 «Объединения бизнесов»</li>
+        <li>МСФО (IFRS) 11 «Совместное предпринимательство»</li>
+        <li>МСФО (IAS) 12 «Налоги на прибыль»</li>
+        <li>МСФО (IAS) 23 «Затраты по заимствованиям»</li>
+    </ul>
+    <li>МСФО (IFRS) 16 Аренда. Особенности применения стандарта начиная с 1 января 2019</li>
+    <li>Новые стандарты и интерпретации (краткий обзор) с 1 января 2018 года</li>
+    <ul>
+    <li>МСФО (IFRS) 9 «Финансовые инструменты»</li>
+    <li>КИМФО (IFRIC) 22 «Операции с иностранной валютой и предварительная
+оплата»</li>
+    <li>«Переводы инвестиционной недвижимости» — изменения в МСФО (IAS) 40
+«Инвестиционное имущество»</li>
+    <li>«Классификация и оценка выплат на основе акций» − поправки к МСФО
+(IFRS) 2</li>
+    </ul>
+    <li>Создание резерва по сомнительным долгам, в связи с переходом от модели
+понесенных кредитных убытков к модели ожидаемых кредитных убытков по
+МСФО (IFRS) 9</li>
+    <li>МСФО (IFRS) 15 Выручка по договорам с покупателями. Особенности
+применения.</li>
+    <li>Изменения в учете основных средств (МСФО (IAS) 16)</li>
+    <li>Изменения в учете инвестиционного имущества (МСФО (IAS) 40)</li>
+    </ol>
+        </div></div>
+		<hr>
+	    <div class="row">
+		    <div class="col-md-3">
+		<h3>Кто Ведет?</h3>
+		    </div>	
+<div class="col-md-9 boxborder">
+	<img class="consultant rounded-circle" style="background:url('images/kolesnikova.jpg') top center/cover;"/>
+		    <h4>Е.В. Михалева</h4>
+			<p>Бизнес - тренер, высококвалифицированный специалист
+с большим практическим опытом работы в области бухгалтерского учета, эксперт в области
+бухгалтерского учета и МСФО, член палаты аудиторов РК, член экспертного совета БИКО,
+сертификаты Всемирного Банка</p>		
+		</div>
+</div>
+	    
+<hr>
+<section class="container cost center">
+<div class="row">
+<div class="col-md-6">
+<h4>Продолжительность</h4>
+<i class="fa fa-clock-o fa-5x" aria-hidden="true"></i>
+<h6><strong>1 день</strong><br>с 11:00 до 13:00 - занятия<br>с 13:00 до 14:00 - обед<br>с 14:00 до 15:30 занятия (15 минут перерыв на кофе-брейк)<br>с 15:45 по 17:00 занятия</h6>
+</div><!-- /.col-md-6 -->
+<div class="col-md-6">
+<h4>Стоимость участия:</h4>
+<i class="fa fa-money fa-5x" aria-hidden="true"></i>
+<h6>23 000 (двадцать три тысячи) тенге/ 1 слушатель
+<br>НДС не облагается</h6>
+</div><!-- /.col-md-6 -->
+</div>
+</section>
+<hr>
+<section class="container marketing cost center">
+		    <h4>Стоимость включает:</h4>
+		    <div class="row">
+          <div class="col-lg-3">
+            <i class="fa fa-graduation-cap fa-5x" aria-hidden="true"></i>
+            <h6>Обучение</h6><p>(в режиме закрытой группы)</p>
+          </div><!-- /.col-lg-3 -->
+		  <div class="col-lg-3">
+            <i class="fa fa-book fa-5x" aria-hidden="true"></i>
+            <h6>Трудовой Кодекса РК</h6><p>(в последней редакции)</p>
+          </div><!-- /.col-lg-3 -->
+          <div class="col-lg-3">
+            <i class="fa fa-clipboard fa-5x" aria-hidden="true"></i>
+            <h6>Учебные материалы по теме</h6>
+          </div><!-- /.col-lg-3 -->
+          <div class="col-lg-3">
+            <i class="fa fa-check-circle fa-5x" aria-hidden="true"></i>
+            <h6>Все налоговые сборы</h6>
+          </div><!-- /.col-lg-3 -->
+		    </div>
+	    </section>
+
+<hr>
+<div class="container">
+<div class="row">
+    <div class="col-md-3"></div>
+    <div class="col-md-6"><a href="https://form.jotform.com/62335883866166" target="_blank" class="register">Принять участие</a></div>
+    <div class="col-md-3"></div>
+</div>
+</div>
+</div>
+</main>
+<?php include("includes/footer.html"); ?>
