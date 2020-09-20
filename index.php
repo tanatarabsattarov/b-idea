e4000a242b9988fedb92a0a6dfb5db4c8322d7b9
Tanatar <tanatar777@mail.ru>
2020-09-20 10:07:12 +0600

    update

--- index.php
+++ index.php
@@ -92,22 +92,22 @@
 	
 		<div class="row">
 		<div class="col-md-3">
-		<a href="vahta_gileva.php" class="program-announce__img-container" style="background-size:60%;background-position:top right;background-image:url(../images/gileva.jpg);">
+		<a href="trudovoi_kodeks_k.php" class="program-announce__img-container" style="background-size:60%;background-position:top right;background-image:url(../images/kolesnikova.jpg);">
 			<div class="program-announce__date">
-				<div class="program-announce__day">15</div>
-				<div class="program-announce__mounth">апреля</div>
+				<div class="program-announce__day">14</div>
+				<div class="program-announce__mounth">октября</div>
 			</div>
 		</a>
 		</div>
 		<div class="col-md-7">
-		<a class="program-announce__middle" href="vahta_gileva.php">
+		<a class="program-announce__middle" href="trudovoi_kodeks_k.php">
 			<div class="program-announce__speaker">
 				<span class="program-announce__speaker-link">
-				Гилева Наталья
+				Марина Колесникова
 				</span>
 			</div>
 			<div class="program-announce__city">
-				<span class="text-color_brown">Актау</span>
+				<span class="text-color_brown">ОНЛАЙН</span>
 			</div>
 			<div class="program-announce__theme">
 				<span class="text-color_brown">
@@ -115,12 +115,47 @@
 				</span>
 			</div>
 			<div class="program-announce__name">
-				<span class="js-font-size">Вахтовый метод: особенности регулирования трудовых отношений</span>
+				<span class="js-font-size">Трудовой Кодекс РК (с изменениями в 2020 году)</span>
 			</div>
 		</a>
 		</div>
 		<div class="col-md-2 more_info_parent">
-		<a class="more_info" href="vahta_gileva.php">Подробнее</a>
+		<a class="more_info" href="trudovoi_kodeks_k.php">Подробнее</a>
+		</div>
+	</div>
+
+
+	<div class="row">
+		<div class="col-md-3">
+		<a href="mediativnye_kompetencii.php" class="program-announce__img-container" style="background-size:60%;background-position:top right;background-image:url(../images/vigovskaya.jpg);">
+			<div class="program-announce__date">
+				<div class="program-announce__day">14</div>
+				<div class="program-announce__mounth">октября</div>
+			</div>
+		</a>
+		</div>
+		<div class="col-md-7">
+		<a class="program-announce__middle" href="mediativnye_kompetencii.php">
+			<div class="program-announce__speaker">
+				<span class="program-announce__speaker-link">
+				Виговская Ирина
+				</span>
+			</div>
+			<div class="program-announce__city">
+				<span class="text-color_brown">ОНЛАЙН</span>
+			</div>
+			<div class="program-announce__theme">
+				<span class="text-color_brown">
+				Семинар
+				</span>
+			</div>
+			<div class="program-announce__name">
+				<span class="js-font-size">Трудовой Кодекс РК (с изменениями в 2020 году)</span>
+			</div>
+		</a>
+		</div>
+		<div class="col-md-2 more_info_parent">
+		<a class="more_info" href="mediativnye_kompetencii.php">Подробнее</a>
 		</div>
 	</div>
 	
