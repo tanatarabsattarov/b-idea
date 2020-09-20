d29e0abfb7c220f7b6e423ad094a4fb129bd3f61
Tanatar <tanatar777@mail.ru>
2020-09-20 11:16:07 +0600

    update

--- index.php
+++ index.php
@@ -174,6 +174,45 @@
 		</div>
 	</div>
 	
+	<hr>
+
+	<div class="row">
+		<div class="col-md-3">
+		<a href="soglasitelnaya_komissiya_3.php" class="program-announce__img-container" style="background-size:60%;background-position:top right;background-image:url(../images/kolesnikova.jpg);">
+			<div class="program-announce__date">
+				<div class="program-announce__day">18</div>
+				<div class="program-announce__mounth">ноября</div>
+			</div>
+		</a>
+		</div>
+		<div class="col-md-7">
+		<a class="program-announce__middle" href="soglasitelnaya_komissiya_3.php">
+			<div class="program-announce__speaker">
+				<span class="program-announce__speaker-link">
+				Марина Колесникова
+				</span>
+			</div>
+			<div class="program-announce__city">
+				<span class="text-color_brown">ОНЛАЙН</span>
+			</div>
+			<div class="program-announce__theme">
+				<span class="text-color_brown">
+				Семинар
+				</span>
+			</div>
+			<div class="program-announce__name">
+				<span class="js-font-size">ИНДИВИДУАЛЬНЫЕ ТРУДОВЫЕ СПОРЫ.
+СОГЛАСИТЕЛЬНАЯ КОМИССИЯ.<br>с учетом изменений, внесенных в Трудовой кодекс РК в мае 2020 г.</span>
+			</div>
+		</a>
+		</div>
+		<div class="col-md-2 more_info_parent">
+		<a class="more_info" href="soglasitelnaya_komissiya_3.php">Подробнее</a>
+		</div>
+	</div>
+
+
+
 </div>
 </section>
 
