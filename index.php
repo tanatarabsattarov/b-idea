f8c676e0fdbdd05f00935ac2bcc43d011fa16054
Tanatar <tanatar777@mail.ru>
2020-10-06 10:12:42 +0600

    update

--- index.php
+++ index.php
@@ -254,6 +254,43 @@
 	</div>
 
 
+	<hr>
+
+<div class="row">
+	<div class="col-md-3">
+	<a href="#" class="program-announce__img-container" style="background-size:60%;background-position:top right;background-image:url(../images/krasnova1.jpg);">
+		<div class="program-announce__date">
+			<div class="program-announce__day">13</div>
+			<div class="program-announce__mounth">ноября</div>
+		</div>
+	</a>
+	</div>
+	<div class="col-md-7">
+	<a class="program-announce__middle" href="#">
+		<div class="program-announce__speaker">
+			<span class="program-announce__speaker-link">
+			Наталья Краснова
+			</span>
+		</div>
+		<div class="program-announce__city">
+			<span class="text-color_brown">ОНЛАЙН</span>
+		</div>
+		<div class="program-announce__theme">
+			<span class="text-color_brown">
+			Мастер класс
+			</span>
+		</div>
+		<div class="program-announce__name">
+			<span class="js-font-size">Организация работ при дистанционном формате</span>
+		</div>
+	</a>
+	</div>
+	<div class="col-md-2 more_info_parent">
+	<a class="more_info" href="#">Подробнее</a>
+	</div>
+</div>
+
+
 
 	<hr>
 
@@ -261,7 +298,7 @@
 		<div class="col-md-3">
 		<a href="soglasitelnaya_komissiya_3.php" class="program-announce__img-container" style="background-size:60%;background-position:top right;background-image:url(../images/kolesnikova.jpg);">
 			<div class="program-announce__date">
-				<div class="program-announce__day">18</div>
+				<div class="program-announce__day">17</div>
 				<div class="program-announce__mounth">ноября</div>
 			</div>
 		</a>
